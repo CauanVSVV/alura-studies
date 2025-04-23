16fb0f44c8f09463065531c1d07ba84a167cc3dd
Cauan VSVV <cauan.valadares.svv@gmail.com>
2025-04-23 17:03:53 -0300

    feat: add task management features with UUID support; refactor components for better state handling and styling

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 import style from "./Botao.module.scss";
 
-interface BotaoProps {
-    children: React.ReactNode;
-}
 
-class Botao extends React.Component<BotaoProps> {
+class Botao extends React.Component<React.PropsWithChildren<{
+    type?: "button" | "submit" | "reset" | undefined,
+    onClick?: () => void,
+
+}>> {
     render() {
-
+        const { type = "button", onClick} = this.props;
 
         return (
-            <button className={style.botao}>
+            <button onClick={onClick} type={type} className={style.botao}>
                 {this.props.children}
             </button>
         )
